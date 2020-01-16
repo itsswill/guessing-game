7d3dfab5f5b0e6e6c8fdb888a5313b7b2e8d8df8
itsswill <willn2doit@gmail.com>
2020-01-16 10:20:23 -0500

    imported necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, NgForm} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    AlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
