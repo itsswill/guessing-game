0f3369d70b3333370b988a9e56e5ad4d1593de04
itsswill <willn2doit@gmail.com>
2020-01-16 10:19:34 -0500

    generated alert component

---
+++ src/app/components/alert/alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../../services/alert.service';
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.scss']
+})
+export class AlertComponent implements OnInit {
+
+  constructor(private alertServ: AlertService) { }
+  ngOnInit() {
+  }
+}
